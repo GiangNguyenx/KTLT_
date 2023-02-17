c45b5072f5ca8832fce758a92339731ff29d297e
Nam Nguyen <meomaythongminh3@gmail.com>
2023-02-17 12:39:14 +0700

    Thêm: Cách lấy chỉ số tách từ dòng trong file vào biến

--- BTL1/knight.cpp
+++ BTL1/knight.cpp
@@ -5,8 +5,17 @@
 #include <string.h>
 #include <sstream>
 
-void readFile(string fileName)
+void readFile(string fileName, int line1_param[], int line2_param[], string line3_param[])
 {
+    /* Các tham số của hàm:
+        - fileName: tên file input
+        - line1_param: dòng 1 trong file input - các chỉ số của hiệp sĩ
+        - line2_param: dòng 2 trong file input - các sự kiện
+        - line3_param: dòng 3 trong file input - các tên file thêm
+        Lưu các chỉ số tách được vào các mảng tương ứng.
+        Tại hàm adventureToKoopa(), khai báo 3 mảng sau đó truyền vào readFile().
+        Các thông số HP, ... sẽ lấy từ mảng sau khi chạy hàm. Đã có ví dụ phía dưới.
+    */
     ifstream file_input(fileName);
     string line;
     string str;
@@ -18,14 +27,18 @@ void readFile(string fileName)
         }
     }
 }
-void round0(){
 
+void round0()
+{
+    int levelO[];
+    for (int i = 1; i <= 5; i++)
+    {
     }
-int levelO[];
-for(int i=1;i<=5;i++){}
-    void round1(){
+}
 
-    }
+void round1()
+{
+}
 
 void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue)
 {
@@ -35,11 +48,16 @@ void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int
          << ", maidenkiss=" << maidenkiss
          << ", phoenixdown=" << phoenixdown
          << ", rescue=" << rescue << endl;
-    
-    
 }
 
 void adventureToKoopa(string file_input, int &HP, int &level, int &remedy, int &maidenkiss, int &phoenixdown, int &rescue)
 {
+    int knight_param[5], event_param[5];
+    string file_param[3];
+
+    readFile(file_input, knight_param, event_param, file_param);
+    HP = knight_param[0];
+
+
     cout << "Function isn't implemented" << endl;
 }
--- BTL1/knight.h
+++ BTL1/knight.h
@@ -3,7 +3,7 @@
 
 #include "main.h"
 
-void readFile(string fileName);
+void readFile(string fileName, int line1_param[], int line2_param[], string line3_param[]);
 void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue);
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue);
 
