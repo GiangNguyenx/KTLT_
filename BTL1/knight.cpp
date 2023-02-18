37b2301a67cbc8175c53d67fcabae0e10127902c
Nam Nguyen <meomaythongminh3@gmail.com>
2023-02-18 13:01:22 +0700

    Test

--- BTL1/knight.cpp
+++ BTL1/knight.cpp
@@ -10,54 +10,40 @@ int levelO=i > 6?(b > 5?b : 5) : b;
 
 void readFile(string fileName, int line1_param[], int line2_param[], string line3_param[])
 {
-    /* Các tham số của hàm:
-        - fileName: tên file input
-        - line1_param: dòng 1 trong file input - các chỉ số của hiệp sĩ
-        - line2_param: dòng 2 trong file input - các sự kiện
-        - line3_param: dòng 3 trong file input - các tên file thêm
+    /* 
+        Các tham số của hàm:
+            - fileName: tên file input
+            - line1_param: dòng 1 trong file input - các chỉ số của hiệp sĩ
+            - line2_param: dòng 2 trong file input - các sự kiện
+            - line3_param: dòng 3 trong file input - các tên file thêm
         Lưu các chỉ số tách được vào các mảng tương ứng.
         Tại hàm adventureToKoopa(), khai báo 3 mảng sau đó truyền vào readFile().
         Các thông số HP, ... sẽ lấy từ mảng sau khi chạy hàm. Đã có ví dụ phía dưới.
     */
-    int count=0;
     ifstream file_input(fileName);
-    string line1, line2, line3;
-    string str1, str2, str3;
-    while (getline(file_input, str1))
+    string line;
+    string str;
+    while (getline(file_input, str))
     {
-        stringstream knight(str1);
-        while (knight >> line1)
-        line1_param[count]=stoi(line1);
-    }
-    while (getline(file_input, str2))
-    {
-        stringstream event(str2);
-        while (event >> line2)
-        line2_param[count]=stoi(line2);
-    }
-    while (getline(file_input, str3))
-    {
-        stringstream file(str3);
-        while (file >> line3)
-        line3_param[count]=stoi(line3);
+        stringstream ss(str);
+        while (ss >> line)
+        {
+        }
     }
 }
 
 void round0()
 {
-    
+    int levelO[];
+    for (int i = 1; i <= 5; i++)
+    {
+    }
 }
 
-void round1(int i, int b, int levelO, )
+void round1()
 { int i=1;
   int b=i%10;
   int levelO=i > 6?(b > 5?b : 5) : b;
-  if(level>levelO) level+=1;
-  else if(level==levelO) continue;
-  else {
-    damage= basedamage*levelO*10;
-    HP= HP - damage;
-  }
   
 }
 
