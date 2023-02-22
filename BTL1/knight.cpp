344409b928da63cd80d1e24103f8d46d2994c72b
Nam Nguyen <75629713+hnamng2301@users.noreply.github.com>
2023-02-23 05:36:50 +0700

    update

--- BTL1/knight.cpp
+++ BTL1/knight.cpp
@@ -4,9 +4,9 @@
 #include <fstream>
 #include <string.h>
 #include <sstream>
-int i = 1;
-int b = i % 10;
-int levelO = i > 6 ? (b > 5 ? b : 5) : b;
+// int i = 1;
+// int b = i % 10;
+// int levelO = i > 6 ? (b > 5 ? b : 5) : b;
 
 void readFile(string fileName, int line1_param[], int line2_param[], string line3_param[])
 {
@@ -25,45 +25,50 @@ void readFile(string fileName, int line1_param[], int line2_param[], string line
     string str1, str2, str3;
     int line1_i = 0, line2_i = 0, line3_i = 0, line_i = 0;
     string line, param;
-        
-        while (getline(file_input, line)){
-            stringstream str(line);
-            while (str >> param){
-                switch (line_i)
-                {
-                    case 0:
-                        line1_param[line1_i] = stoi(param);
-                        line1_i++;
-                        break;
-                    case 1:
-                        line2_param[line2_i] = stoi(param);
-                        line2_i++;
-                        break;
-                    case 2:
-                        line3_param[line3_i] = param;
-                        line3_i++;
-                        break;
-                    default:
-                        break;
-                }
+
+    while (getline(file_input, line))
+    {
+        stringstream str(line);
+        while (str >> param)
+        {
+            switch (line_i)
+            {
+            case 0:
+                line1_param[line1_i] = stoi(param);
+                line1_i++;
+                break;
+            case 1:
+                line2_param[line2_i] = stoi(param);
+                line2_i++;
+                break;
+            case 2:
+                line3_param[line3_i] = param;
+                line3_i++;
+                break;
+            default:
+                break;
             }
-            line_i++;
         }
-   
+        line_i++;
+    }
 }
 
-void initArray(int knight_param[], int event_param[], string file_param[]){
-    for(int i = 0; i < 5; i++){
+void initArray(int knight_param[], int event_param[], string file_param[])
+{
+    for (int i = 0; i < 5; i++)
+    {
         knight_param[i] = -10000;
         event_param[i] = -1;
     }
 
-    for(int i = 0; i < 3; i++){
+    for (int i = 0; i < 3; i++)
+    {
         file_param[i] = "";
     }
 }
 
-void initKnight(int knight_param[], int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue){
+void initKnight(int knight_param[], int &HP, int &level, int &remedy, int &maidenkiss, int &phoenixdown, int &rescue)
+{
     HP = knight_param[0];
     level = knight_param[1];
     remedy = knight_param[2];
@@ -71,42 +76,127 @@ void initKnight(int knight_param[], int & HP, int & level, int & remedy, int & m
     phoenixdown = knight_param[4];
 }
 
-void eventZero(int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue){
+void eventZero(int &HP, int &level, int &remedy, int &maidenkiss, int &phoenixdown, int &rescue)
+{
+    rescue = 1;
     return;
 }
-    
-void traverseEvent(int event_param[], int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue){
-    for (int event = 0; event < 5; event++){
-        if (event == 0) {
+
+void eventOneToFive(int event_code, int event, int maxHP, int &HP, int &level, int &remedy, int &maidenkiss, int &phoenixdown, int &rescue)
+{
+    int b = event % 10;
+    int levelO = event > 6 ? (b > 5 ? b : 5) : b; // i ~ event
+
+    // cout << b << ' ' << levelO << ' ' << level << ' ' << event << endl;
+    if (level > levelO)
+    {
+        if (level == 10)
+        {
+        }
+        else
+        {
+            level++;
+        }
+    }
+    else if (level == levelO)
+    {
+    }
+    else
+    {
+        float damage, baseDamage;
+        switch (event_code)
+        {
+        case 1:
+            baseDamage = 1;
+            break;
+        case 2:
+            baseDamage = 1.5;
+            break;
+        case 3:
+            baseDamage = 4.5;
+            break;
+        case 4:
+            baseDamage = 7.5;
+            break;
+        case 5:
+            baseDamage = 9.5;
+            break;
+        default:
+            break;
+        }
+
+        damage = baseDamage * levelO * 10;
+        cout << damage << ' ' << levelO << baseDamage << endl;
+        HP = HP - (int)damage;
+        if (HP <= 0)
+        {
+            if (phoenixdown == 0)
+            {
+                rescue = 0;
+                return;
+            }
+            else if (phoenixdown > 0)
+            {
+                phoenixdown--;
+                HP = maxHP;
+                if (event < 4)
+                    rescue = -1;
+                else rescue = 1;
+            }
+        }
+    }
+    // rescue = 1;
+}
+
+void traverseEvent(int event_param[], int &HP, int &level, int &remedy, int &maidenkiss, int &phoenixdown, int &rescue)
+{
+    int maxHP = HP;
+    bool is_rescued = false;
+    rescue = -1;
+
+    for (int event = 0; event < 5; event++)
+    {
+        int event_code = event_param[event];
+        if (event_code == -1) break;
+        if (event_code == 0)
+        {
             eventZero(HP, level, remedy, maidenkiss, phoenixdown, rescue);
             return;
         }
-        else if (event == 1 || event == 2 || event == 3 || event == 4 || event == 5) {
-            eventOneToFive(HP, level, remedy, maidenkiss, phoenixdown, rescue);
+        else if (event_code == 1 || event_code == 2 || event_code == 3 || event_code == 4 || event_code == 5)
+        {
+            eventOneToFive(event_code, event + 1, maxHP, HP, level, remedy, maidenkiss, phoenixdown, rescue);
+            if (rescue == -1)
+                return;
         }
-        // 
+        //
     }
-}   
 
-void round0(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue){
-    rescue=1;
+    // if (is_rescued) rescue = 1;
+    // else if (!is_rescued) rescue = -1;
+    // else rescue = 0;
 }
 
-void round1(int i, int b, int levelO,int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue )
+void round0(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue)
 {
-    int i = 1, basedamage=1, damage;
-    int b = i % 10;
-    int levelO = i > 6 ? (b > 5 ? b : 5) : b;
-    if (level > levelO){
-        if(level<10) level+=1;
-    }
-    else if (level == levelO)
-        level+=0;
-    else
-    {
-        damage = basedamage * levelO * 10;
-        HP = HP - damage;
-    }
+    rescue = 1;
+}
+
+void round1(int i, int b, int levelO, int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue)
+{
+    // int i = 1, basedamage=1, damage;
+    // int b = i % 10;
+    // int levelO = i > 6 ? (b > 5 ? b : 5) : b;
+    // if (level > levelO){
+    //     if(level<10) level+=1;
+    // }
+    // else if (level == levelO)
+    //     level+=0;
+    // else
+    // {
+    //     damage = basedamage * levelO * 10;
+    //     HP = HP - damage;
+    // }
 }
 
 void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue)
@@ -126,7 +216,7 @@ void adventureToKoopa(string file_input, int &HP, int &level, int &remedy, int &
 
     initArray(knight_param, event_param, file_param);
     readFile(file_input, knight_param, event_param, file_param);
-    // cout << phoenixdown << endl;
+    // cout << file_param[1] << endl;
 
     // ******* Knight parameter *********
     initKnight(knight_param, HP, level, remedy, maidenkiss, phoenixdown, rescue);
